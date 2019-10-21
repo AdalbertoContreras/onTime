ba26234983a4bdcbc6ffba7f20e2c867fafda0b6
Adalberto <you@example.comandis114@hotmail.com>
2019-10-21 09:57:49 +0200

    Agregar archivos de proyecto.

---
+++ onTime/Asignar_domicilio.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace onTime
+{
+    /// <summary>
+    /// Lógica de interacción para Asignar_ticket.xaml
+    /// </summary>
+    public partial class Asignar_ticket : Window
+    {
+        public Asignar_ticket()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ onTime/Registrar_mensajero.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace onTime
+{
+    /// <summary>
+    /// Lógica de interacción para Registrar_mensajero.xaml
+    /// </summary>
+    public partial class Registrar_mensajero : Window
+    {
+        public Registrar_mensajero()
+        {
+            InitializeComponent();
+        }
+    }
+}
